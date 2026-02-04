4f668b32460aafcacfa11ded0f67fe8249b29890
Guillermo Valarezo <gvalarezog@gmail.com>
2026-02-03 19:39:44 -0500

    documentación del código

--- 2026_Inv_Funciones.py
+++ 2026_Inv_Funciones.py
@@ -1,4 +1,11 @@
 def calcular_area(figura, ancho, alto):
+    '''
+    Esta función calcula el area de la figura y el ancho de la alto
+    :param figura: puede ser cuadrado, rectangulo, triangulo
+    :param ancho: valor del ancho en numero positivos
+    :param alto: valor del alto en numero positivos
+    :return: una cadena de texto con el calculo del area correspondiente al figura
+    '''
     figura_revisada = figura.lower().strip()
     area = 1
     if figura_revisada == 'cuadrado' or figura_revisada == 'rectangulo':
